a3105cfc43c1bca534063481f8cae54af6520389
fwu0216 <62977890+fwu0216@users.noreply.github.com>
2025-07-14 13:36:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -152,3 +152,8 @@ if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
+    
+#if __name__ == '__main__':
+ #   import os
+  #  port = int(os.environ.get('PORT', 10000))
+   # app.run(host='0.0.0.0', port=port, debug=False)
