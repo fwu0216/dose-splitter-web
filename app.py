6770e4eb29e50aad1c339499c0e408b1e2853f31
fwu0216 <62977890+fwu0216@users.noreply.github.com>
2025-07-14 12:33:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -127,4 +127,6 @@ def index():
                                   result_volume=result_volume)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
